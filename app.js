5449770bd574a7220904c0e2322bcdff192f9fab
ansharahamin <insharahamin1250@gmail.com>
2025-09-25 17:58:17 +0500

    Remaining assignment

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // -- Question 01 --
+// function tellTime() {
+//   var now = new Date();
+//   alert("Today: " + now);
+// }
+// tellTime();
+
+// -- Question 02 --
+// function greetByUserName(){
+// var fName = prompt("Enter your first name :");
+// var LName = prompt("Enter your last name :");
+// var fullName = fName + " " + LName; ;
+// alert("Hello, " + fullName + "!");
+// }
+// greetByUserName();
+
+// -- Question 03 --
+// function addTwoNumbers() {
+//   var num1 = parseInt(prompt("Enter the first number:"));
+//   var num2 = parseInt(prompt("Enter the second number:"));
+//   var sum = num1 + num2;
+//   return sum;
+// }
+// var result = addTwoNumbers();
+// alert("The sum is: " + result);
+
+// -- Question 04 --
+// function calculator(num1, num2, operator) {
+//   if (operator === "+") {
+//     console.log(num1 + num2);
+//   } else if (operator === "-") {
+//     console.log(num1 - num2);
+//   } else if (operator === "/") {
+//     console.log(num1 / num2);
+//   } else if (operator === "*") {
+//     console.log(num1 * num2);
+//   } else {
+//     alert("Invalid Operator");
+//   }
+// }
+// var userValue1 = Number(prompt("Enter num1"));
+// var userValue2 = Number(prompt("Enter num2"));
+// var userOpr = prompt("Enter operator  (+, -, *, /):");
+// calculator(userValue1, userValue2, userOpr);
+
+
+
